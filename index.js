67e7f7b115d8566e6d900a3d313bc5ced597fd5c
Uladzimir Piatruška <uladzimir@mail.mail>
2019-07-20 13:32:30 +0300

    linting the code

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const farbs = ['red', 'blue'];
 let clBrush = farbs[0];
-let divFarb = document.getElementsByClassName("farba");
-for (let i = 0; i < divFarb.length; i++){
-divFarb[i].style.backgroundColor = farbs[i]};
-let farbItems = document.getElementsByClassName("farba");
-for (let i = 0; i < farbItems.length; i++){
-farbItems[i].addEventListener('click',
-function(){clBrush = this.style.backgroundColor;});    
+const divFarb = document.getElementsByClassName('farba');
+
+for (let i = 0; i < divFarb.length; i++) {
+  divFarb[i].style.backgroundColor = farbs[i];
 }
-let divItems = document.getElementsByClassName("xx");
+const farbItems = document.getElementsByClassName('farba');
+
+for (let i = 0; i < farbItems.length; i++) {
+  farbItems[i].addEventListener('click', function() { clBrush = this.style.backgroundColor; });
+}
+const divItems = document.getElementsByClassName('xx');
+
 for (let i = 0; i < divItems.length; i++) {
-divItems[i].addEventListener('click',
-function(){this.style.backgroundColor = clBrush;});
+  divItems[i].addEventListener('click',
+    function() { this.style.backgroundColor = clBrush; });
 }
